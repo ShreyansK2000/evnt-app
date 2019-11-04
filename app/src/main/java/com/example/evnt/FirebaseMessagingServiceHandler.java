ba683421f8640f3a30a7ec3a63815f5bf9594ca5
ShreyansK2000 <gshreyansk4@gmail.com>
2019-11-04 14:22:25 -0800

    codacy styling changes part 7

--- app/src/main/java/com/example/evnt/FirebaseMessagingServiceHandler.java
+++ app/src/main/java/com/example/evnt/FirebaseMessagingServiceHandler.java
@@ -16,17 +16,17 @@
 
 package com.example.evnt;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
+//import android.app.NotificationChannel;
+//import android.app.NotificationManager;
+//import android.app.PendingIntent;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.media.RingtoneManager;
+//import android.net.Uri;
+//import android.os.Build;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.NotificationCompat;
+//import androidx.core.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;

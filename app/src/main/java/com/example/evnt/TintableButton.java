11c68bd34bfdc577b776f50f17e4ef1cad239b54
ShreyansK2000 <gshreyansk4@gmail.com>
2019-11-25 11:56:09 -0800

    the 'button' can now be 'clicked'

--- app/src/main/java/com/example/evnt/TintableButton.java
+++ app/src/main/java/com/example/evnt/TintableButton.java
@@ -1,9 +1,11 @@
 package com.example.evnt;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.animation.AnimationUtils;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -27,6 +29,7 @@ public class TintableButton extends CircleImageView {
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN && !mIsSelected) {
             setColorFilter(0x99000000);
+            this.startAnimation(AnimationUtils.loadAnimation(this.getContext(), R.anim.image_click));
             mIsSelected = true;
         } else if ((event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_CANCEL) && mIsSelected) {
             setColorFilter(Color.TRANSPARENT);
--- app/src/main/java/com/example/evnt/fragments/PickEvntFragment.java
+++ app/src/main/java/com/example/evnt/fragments/PickEvntFragment.java
@@ -52,6 +52,8 @@ public class PickEvntFragment extends Fragment {
     private FusedLocationProviderClient fusedLocationClient;
     private int locationPermissionGranted;
     private ServerRequestModule mServerRequestModule;
+    private CircleImageView imageView;
+    private int clickCount;
 //
 //    public static PickEvntFragment newInstance(ServerRequestModule serverRequestModule) {
 //        PickEvntFragment fragment = new PickEvntFragment();
@@ -82,6 +84,7 @@ public class PickEvntFragment extends Fragment {
             Toast.makeText(context, "serverProblem", Toast.LENGTH_LONG).show();
         }
         setHasOptionsMenu(true);
+        clickCount = 0;
     }
 
 
@@ -94,7 +97,7 @@ public class PickEvntFragment extends Fragment {
         final View view = inflater.inflate(R.layout.fragment_pickevnt,
                 container, false);
 
-        CircleImageView imageView = view.findViewById(R.id.evnt_button);
+        imageView = view.findViewById(R.id.evnt_button);
         imageView.setOnClickListener(mainClickListener);
 
 
@@ -110,50 +113,61 @@ public class PickEvntFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
-            getLocation(new GetLocationCallback() {
-                @Override
-                public void gotLocationString(final String location) {
-                    String url = "https://api.evnt.me/events/api/suggest/";
-                    mServerRequestModule.getBestEvent(url, location, new VolleyBestEventCallback() {
-                        @Override
-                        public void onReceivedBestEvent(JSONObject response) {
-                            // do stuff with response
-                            EvntCardInfo evnt = null;
-                            try {
-                                JSONObject data = (JSONObject) response.get("data");
-                                 evnt = new EvntCardInfo.Builder()
-                                        .withName(data.get("name").toString())
-                                        .withDescription((String) data.get("description"))
-                                        .withStartTime((String) data.get("startTime"))
-                                        .withEndTime((String) data.get("endTime"))
-                                        .withLocation((String) data.get("location"))
-                                        .withId((String) data.get("_id"))
-                                        .withHostId(data.get("host").equals(ident.getValue(context.getString(R.string.user_id))) ? "you" : "Anonymous")
-                                         .withHostName((data.get("hostname")).toString())
-                                         .withTagList((data.get("tagList").toString().replace("[","")
-                                                .replace("]","").replace("\"", "")).split(","))
-                                        .build();
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                            if (evnt != null) {
-                                EvntDetailsDialog detailsDialog = new EvntDetailsDialog(context,
-                                        evnt.getEvntName(), evnt.getDateString(), evnt.getDescription(),
-                                        evnt.getLocation(), mServerRequestModule, evnt.getId(), evnt.getImage(), evnt.getTagList(),
-                                        evnt.getHostName());
-                                detailsDialog.show(getFragmentManager(), "");
-                            }
-                        }
+            imageView.setClickable(false);
+            clickCount ++;
+            if (clickCount == 1) {
+                getLocation(new GetLocationCallback() {
+                    @Override
+                    public void gotLocationString(final String location) {
+                        String url = "https://api.evnt.me/events/api/suggest/";
+                        mServerRequestModule.getBestEvent(url, location, new VolleyBestEventCallback() {
+                            @Override
+                            public void onReceivedBestEvent(JSONObject response) {
+                                // do stuff with response
+                                EvntCardInfo evnt = null;
+                                try {
+                                    JSONObject data = (JSONObject) response.get("data");
+                                    evnt = new EvntCardInfo.Builder()
+                                            .withName(data.get("name").toString())
+                                            .withDescription((String) data.get("description"))
+                                            .withStartTime((String) data.get("startTime"))
+                                            .withEndTime((String) data.get("endTime"))
+                                            .withLocation((String) data.get("location"))
+                                            .withId((String) data.get("_id"))
+                                            .withHostId(data.get("host").equals(ident.getValue(context.getString(R.string.user_id))) ? "you" : "Anonymous")
+                                            .withHostName((data.get("hostname")).toString())
+                                            .withTagList((data.get("tagList").toString().replace("[", "")
+                                                    .replace("]", "").replace("\"", "")).split(","))
+                                            .build();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                if (evnt != null) {
+                                    EvntDetailsDialog detailsDialog = new EvntDetailsDialog(context,
+                                            evnt.getEvntName(), evnt.getDateString(), evnt.getDescription(),
+                                            evnt.getLocation(), mServerRequestModule, evnt.getId(), evnt.getImage(), evnt.getTagList(),
+                                            evnt.getHostName());
+                                    detailsDialog.show(getFragmentManager(), "");
+                                    imageView.setClickable(true);
+                                    clickCount = 0;
+                                }
 
-                        @Override
-                        public void onErrorResponse(String error) {
-                            Toast.makeText(context.getApplicationContext(),
-                                    "Either no events to suggest, or internal error",
-                                    Toast.LENGTH_LONG).show();
-                        }
-                    });
-                }
-            });
+                            }
+
+                            @Override
+                            public void onErrorResponse(String error) {
+                                Toast.makeText(context.getApplicationContext(),
+                                        "Either no events to suggest, or internal error",
+                                        Toast.LENGTH_LONG).show();
+                                imageView.setClickable(true);
+                                clickCount = 0;
+                            }
+                        });
+                    }
+                });
+            } else {
+                Toast.makeText(context, "Too many clicks", Toast.LENGTH_LONG).show();
+            }
         }
     };
 
